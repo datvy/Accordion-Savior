7fcb539093758402745bf7ca135c013e26fd90cc
carbonellm0040 <59749695+carbonellm0040@users.noreply.github.com>
2020-01-13 14:28:18 -0800

    Added comments

--- Gameboard.java
+++ Gameboard.java
@@ -6,9 +6,9 @@ public class Gameboard extends MyWorld{
     
     private void generateRow(int row) {
         Random rand = new Random();
-        int randPos = rand.nextInt(4);
+        int randPos = rand.nextInt(4);  //generate number between 0 and 4 to determine position in row
             
-        switch(randPos) {
+        switch(randPos) {   //overwrite given row with new note position
             case 0:
                 board[row][0] = true;
                 break;
@@ -25,16 +25,16 @@ public class Gameboard extends MyWorld{
     }
     
     public Gameboard() {
-        for(int i = 0; i < board.length - 1; i++) {
+        for(int i = 0; i < board.length - 1; i++) {     //loop through the entire board array and use generateRow() to fill all of it
             generateRow(i);
         }
     }
     
-    public void updateBoard() {
+    public void updateBoard() {     //generate new row determined by which rows the player cannot see
         generateRow(Player.getScore() % 6);
     }
     
-    public int getNotePos() {
+    public int getNotePos() {   //based on current row, return what position the note in said row is occupying
         int pos = -1;
         for(int i = 0; i < board[0].length - 1; i++) {
             if (board[Player.getScore() % 6][i] == true)
