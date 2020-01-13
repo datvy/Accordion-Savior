cfbf6fa3af0c8102115fd9a37c71bbbf723e18c6
MoyaAine <59749845+MoyaAine@users.noreply.github.com>
2020-01-13 14:20:01 -0800

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,14 +1,21 @@
 public class Game{
-    Player newPlayer;
-    GameBoard newGameBoard;
-    boolean isGoing; //determines whether or not the game is being played
-    Note currentNote;
-    public Game(/*inputs??*/){
+    
+    Player newPlayer;//this is the player in the game
+    GameBoard newGameBoard;//this is the board that is played on
+    boolean isGoing; //this determines whether or not the game is being played
+    Note currentNote;//the current note that the player is supposed to hit
+    
+    public Game(){
         isGoing=false;
         //set currentNote, is there a correlation with gameboard??
     }
-   //code start game method (in main??)
-        //set isGoing to true
+    public boolean getIsGoing(){
+        return isGoing;
+    }
+    public boolean startGame(){
+        isGoing=true;
+        return isGoing;
+    }
     public void endGame(){
         GreenfootImage text=new GreenfootImage(x-size, y-size);
         setImage(text);
@@ -17,12 +24,16 @@ public class Game{
         System.exit(0);//end graphics
     }
     
-    public void isRunning(){
-        if(currentNote.verifyPlayerInput(currentNote.getRow)==false){
+    public boolean keepRunning(){//determines if the game is still going, used to end the game
+        if(currentNote.verifyPlayerInput(currentNote.getRow)==false){//if the player hits the wrong key
             isGoing=false;
-        }else if(key.getX() > 500){
+            return isGoing;
+        }else if(key.getX() > 500){//if the player doesn't hit the key in time
+            isGoing=false;
+            return isGoing;
         }else{
             isGoing=true;
+            return isGoing
         }
         
     }
