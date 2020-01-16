057dca8dadefa9af0e0a177ba4fbd0d91dc30abb
datvy <55923366+datvy@users.noreply.github.com>
2020-01-15 21:06:39 -0800

    Add files via upload

--- GameTest.java
+++ GameTest.java
@@ -5,23 +5,25 @@ import java.util.Random;
 
 public class GameTest extends World{
     
-    Note note0;
-    Note note1;
-    Note note2;
-    Note note3;
-    Note note4;
-    Note note5;
-    Note[] noteArray;
+    private Note note0;
+    private Note note1;
+    private Note note2;
+    private Note note3;
+    private Note note4;
+    private Note note5;
+    private Note[] noteArray;
+    private int timer;
+    private int score;
     
     public GameTest(){
      
         super(600, 400, 1);
-        note0 = new Note(0, false, 5, false);
-        note1 = new Note(0, false, 5, false);
-        note2 = new Note(0, false, 5, false);
-        note3 = new Note(0, false, 5, false);
-        note4 = new Note(0, false, 5, false);
-        note5 = new Note(0, false, 5, false);
+        note0 = new Note(0, false, 3, false);
+        note1 = new Note(0, false, 3, false);
+        note2 = new Note(0, false, 3, false);
+        note3 = new Note(0, false, 3, false);
+        note4 = new Note(0, false, 3, false);
+        note5 = new Note(0, false, 3, false);
         
         noteArray = new Note[6];
         noteArray[0] = note0;
@@ -30,15 +32,54 @@ public class GameTest extends World{
         noteArray[3] = note3;
         noteArray[4] = note4;
         noteArray[5] = note5;
+        
+        timer = 0;
+        score = 0;
     }
     
     public void act() {
     
-        add();
+        if (timer%25==0) {
+        
+            addObject(getNote(), 0, 0);
+            timer = 0;
+            
+            updateScore();
+        
+        }
+        
+        
+        showText("Score: "+score, 40, 20);
+        
+        checkMiss();
+    
+        timer++;
+        
+    }
+    
+    public void updateScore() {
+        
+       score++;
+        
+    }
+    
+    public void checkMiss() {
+    
+        for (int i = 0; i<noteArray.length; i++) {
+        
+            if (noteArray[i].getInUse()&&noteArray[i].getX()==599) {
+            
+                showText("YOUR SCORE: "+score, 300, 200);
+                
+                Greenfoot.stop();
+                
+            }
+        
+        }
     
     }
     
-    public void add() {
+    public Note getNote() {
     
         for (int i = 0; i<noteArray.length; i++) {
         
@@ -47,11 +88,13 @@ public class GameTest extends World{
                 noteArray[i].setRow(randomRow());
                 noteArray[i].setInUse(true);
                 
-                addObject(noteArray[i], 0, 0);
+                return noteArray[i];
             
             }
         
         }
+        
+        return noteArray[randomRow()];
         
     }
     
@@ -59,7 +102,7 @@ public class GameTest extends World{
     
         Random rand = new Random();
         
-        return rand.nextInt(4);
+        return rand.nextInt(100)/25;
     
     }
 }
--- Note.java
+++ Note.java
@@ -1,71 +1,59 @@
-import greenfoot.*;
-public class Note extends Actor{
-    private int row;//the row determines the key
-    private boolean isHit;//true if correct key is hit
-    private boolean inUse;
-    private int scrollSpeed; //the speed the notes scroll at
-    private static int numInUse;
-    //constructor
-    public Note(int row, boolean isHit, int scrollSpeed, boolean inUse){
-        this.row=row;
-        this.isHit=isHit;
-        this.scrollSpeed=scrollSpeed;
-        this.inUse=inUse;
-        numInUse = 0;
-    }
-    
-    //getters and setters
-    public int getRow(){
-        return row;
-    }
-    public boolean getIsHit(){ 
-        return isHit; 
-    }
-    public int getScrollSpeed(){ 
-        return scrollSpeed; 
-    }
-    public boolean getInUse() {
-        return inUse;
-    }
-    public void setRow(int thisRow){ 
-        row=thisRow; 
-    }
-    public void setisHit(boolean changeIsHit){ 
-        isHit=changeIsHit; 
-    }
-    public void setScrollSpeed(int thisScrollSpeed){ 
-        scrollSpeed=thisScrollSpeed; 
-    }
-    public void setInUse(boolean thisInUse) {
-        inUse=thisInUse;
-    }
-    public static void incrementNumInUse() {
-        numInUse++; 
-    }
-    public static void decrementNumInUse() {
-        numInUse--;
-    }
-    public static int getNumInUse() {
-        return numInUse;
-    }
-    //end getters and setters 
-    
-    public void move() {
-    
-        if (inUse) {
-            setLocation(getX() + scrollSpeed, 400/(row+1)+50);
+    import greenfoot.*;
+    public class Note extends Actor{
+        private int row;//the row determines the key
+        private boolean inUse;
+        private int scrollSpeed; //the speed the notes scroll at
+        private final String[] key = {"2","w","s","x"};
+        private int timer;
+        private int cheatTimer;
+        //constructor
+        public Note(int row, boolean isHit, int scrollSpeed, boolean inUse){
+            this.row=row;
+            this.scrollSpeed=scrollSpeed;
+            this.inUse=inUse;
+            timer = 0;
+            cheatTimer = 0;
         }
         
-    }
-    
-    public void incrementSpeed(){//increment speed
-        int inc=5;//the change in the speed, subject to change
-        scrollSpeed += inc;
-    }
-    
-    public boolean hitNote() {
-    
-        if (Greenfoot.isKeyDown(""+row)) {
+        //getters and setters
+        public int getRow(){
+            return row;
+        }
+        public int getScrollSpeed(){ 
+            return scrollSpeed; 
+        }
+        public boolean getInUse() {
+            return inUse;
+        }
+        public void setRow(int thisRow){ 
+            row=thisRow; 
+        }
+        public void setScrollSpeed(int thisScrollSpeed){ 
+            scrollSpeed=thisScrollSpeed; 
+        }
+        public void setInUse(boolean thisInUse) {
+            inUse=thisInUse;
+        }
+        //end getters and setters 
+        
+        public void move() {
+        
+            if (inUse) {
+                setLocation(getX() + scrollSpeed, 100*row+50);
+            }
+            
+        }
+        
+        public void incrementSpeed(){//increment speed
+            int inc=1;//the change in the speed, subject to change
+            scrollSpeed += inc;
+        }
+        
+        public boolean hitNote() {
+            
+        if (this.getInUse()&&Greenfoot.isKeyDown(key[this.row])&&this.getX()>500) {
+            
+            cheatTimer = 0;
             
             return true;
             
@@ -73,9 +61,49 @@ public class Note extends Actor{
        
     }
     
+    public boolean cheatCatch() {
+    
+        if (Greenfoot.isKeyDown(key[this.row])) {
+        
+            cheatTimer++;
+        
+        }
+        
+        if (cheatTimer>20) {
+        
+            cheatTimer = 0;
+            
+            return true;
+        
+        }
+        
+        return false;
+    
+    }
+    
     public void act() {
     
         move();
+        
+        if (hitNote()&&!cheatCatch()) {
+            
+            this.getWorld().removeObject(this);
+        
+            this.setRow((int)Math.random()*3);
+            
+            this.setInUse(false);
+            
+        }
+        
+        if (timer%500==0) {
+        
+            incrementSpeed();
+            
+            timer = 0;
+        
+        }
+        
+        timer++;
         
     }
 }
